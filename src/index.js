0de51c1bee1dbe0580fc183bcc176806d6fe93b9
Balint Soos <soosbalint95@gmail.com>
2017-09-09 11:59:23 +0200

    mongoose setup

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ const app = require('./config/app');
 
 const debug = require('debug')('rezsi-io:index');
 
-mongoose.Promise = global.Promise;
-
 const mongoUri = config.mongo.host;
 
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${mongoUri}`);
-});
-
+mongoose.Promise = global.Promise;
 mongoose.connect(mongoUri, {
+  promiseLibrary: global.Promise,
+  useMongoClient: true,
   keepAlive: true,
+});
+mongoose.connection.on('error', () => {
+  throw new Error(`Unable to connect to database: ${mongoUri}`);
+});
+mongoose.connection.once('open', () => {
+  console.info(`Connected to database: ${mongoUri}`); // eslint-disable-line no-console
 });
 
 if (config.MONGOOSE_DEBUG) {
