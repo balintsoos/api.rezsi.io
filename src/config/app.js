d6faf43a3cbb18e3b815fa01d884d64f920ddb13
Balint Soos <soosbalint95@gmail.com>
2017-09-15 00:52:17 +0200

    debug & logging tools

--- src/config/app.js
+++ src/config/app.js
@@ -5,15 +5,12 @@ const cookieParser = require('cookie-parser');
 const compress = require('compression');
 const methodOverride = require('method-override');
 const cors = require('cors');
-const httpStatus = require('http-status');
 const expressWinston = require('express-winston');
-const expressValidation = require('express-validation');
 const helmet = require('helmet');
 
 const winstonInstance = require('./winston');
 const config = require('./main');
 const routes = require('../routes/index.route');
-const APIError = require('../helpers/APIError');
 
 const app = express();
 
@@ -44,42 +41,5 @@ if (config.env === 'development') {
 }
 
 app.use('/api/v1', routes);
-
-// if error is not an instanceOf APIError, convert it.
-app.use((err, req, res, next) => {
-  if (err instanceof expressValidation.ValidationError) {
-    // validation error contains errors which is an array of error each containing message[]
-    const unifiedErrorMessage = err.errors.map(error => error.messages.join('. ')).join(' and ');
-    const error = new APIError(unifiedErrorMessage, err.status, true);
-    return next(error);
-  } else if (!(err instanceof APIError)) {
-    const apiError = new APIError(err.message, err.status, err.isPublic);
-    return next(apiError);
-  }
-
-  return next(err);
-});
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new APIError('API not found', httpStatus.NOT_FOUND);
-
-  return next(err);
-});
-
-// log error in winston transports except when executing test suite
-if (config.env !== 'test') {
-  app.use(expressWinston.errorLogger({
-    winstonInstance,
-  }));
-}
-
-// error handler, send stacktrace only during development
-app.use((err, req, res) => {
-  const message = err.isPublic ? err.message : httpStatus[err.status];
-  const stack = config.env === 'development' ? err.stack : {};
-
-  return res.status(err.status).json({ message, stack });
-});
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const config = require('./config/main');
 const app = require('./config/app');
 
-const debug = require('debug')('rezsi-io:index');
+const debug = require('debug')('API:index');
 
 const mongoUri = config.mongo.host;
 
@@ -14,12 +14,15 @@ mongoose.connect(mongoUri, {
   useMongoClient: true,
   keepAlive: true,
 });
-mongoose.connection.on('error', () => {
-  throw new Error(`Unable to connect to database: ${mongoUri}`);
-});
-mongoose.connection.once('open', () => {
-  console.info(`Connected to database: ${mongoUri}`); // eslint-disable-line no-console
-});
+
+const db = mongoose.connection;
+
+db.on('connecting', () => debug(`Connecting to to database: ${mongoUri}`));
+db.on('connected', () => debug(`Connected to to database: ${mongoUri}`));
+db.once('open', () => debug(`Connection is open to database: ${mongoUri}`));
+db.on('error', () => debug(`Unable to connect to database: ${mongoUri}`));
+db.on('reconnected', () => debug(`Reconnected to database: ${mongoUri}`));
+db.on('disconnected', () => debug(`Disconnected from database: ${mongoUri}`));
 
 if (config.MONGOOSE_DEBUG) {
   mongoose.set('debug', (collectionName, method, query, doc) => {
@@ -30,9 +33,8 @@ if (config.MONGOOSE_DEBUG) {
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
-  // listen on port config.port
   app.listen(config.port, () => {
-    console.info(`Server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
+    debug(`Server started on port ${config.port} (${config.env})`);
   });
 }
 
