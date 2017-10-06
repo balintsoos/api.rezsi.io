be944cdb8f7900b918f54de99efcc44a80d0ffe2
Balint Soos <soosbalint95@gmail.com>
2017-10-06 20:50:26 +0200

    error messages for auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 const httpStatus = require('http-status');
+const debug = require('debug')('API:auth.controller');
 
 const auth = require('../config/auth');
 const User = require('../models/user.model');
 
 async function getToken(req, res) {
   if (!req.body.email || !req.body.password) {
+    debug('PAYLOAD_VALIDATION_FAILED');
     return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 
@@ -13,20 +15,24 @@ async function getToken(req, res) {
   try {
     user = await User.findOne({ email: req.body.email }).exec();
   } catch (err) {
+    debug('DB_FIND_FAILED %O', err);
     return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 
   if (!user) {
+    debug('CANNOT_FIND_USER');
     return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 
   if (!user.confirmed) {
+    debug('USER_NOT_CONFIRMED');
     return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 
   const result = await user.comparePassword(req.body.password);
 
   if (result === false) {
+    debug('PASSWORD_NOT_MATCH');
     return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 
