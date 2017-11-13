b00587eda1e3e71564bd0bf37bca68fa6de1db97
Balint Soos <soosbalint95@gmail.com>
2017-11-13 14:40:14 +0100

    calculate consuption between dates

--- src/controllers/summary.controller.js
+++ src/controllers/summary.controller.js
@@ -3,6 +3,9 @@ const httpStatus = require('http-status');
 const Summary = require('../models/summary.model');
 const Bill = require('../models/bill.model');
 const User = require('../models/user.model');
+const Report = require('../models/report.model');
+
+const consumption = (current, last, key) => current[key] - last[key];
 
 async function create(req, res) {
   let summary = new Summary(Object.assign({}, req.body, {
@@ -24,10 +27,45 @@ async function create(req, res) {
   }
 
   users.forEach(async (user) => {
+    const reports = await Report
+      .find({
+        user: user.id,
+        createdAt: {
+          $gte: new Date(summary.from),
+          $lte: new Date(summary.to),
+        },
+      })
+      .sort({ createdAt: -1 })
+      .exec();
+
+    const lastReports = await Report
+      .find({
+        user: user.id,
+        createdAt: {
+          $lt: new Date(summary.from),
+        },
+      })
+      .sort({ createdAt: -1 })
+      .exec();
+
+    let consumptions = {
+      hotWater: 0,
+      coldWater: 0,
+      heat: 0,
+    };
+
+    if (reports.length > 0 && lastReports.length > 0) {
+      consumptions = {
+        hotWater: consumption(reports[0], lastReports[0], 'hotWater'),
+        coldWater: consumption(reports[0], lastReports[0], 'coldWater'),
+        heat: consumption(reports[0], lastReports[0], 'heat'),
+      };
+    }
+
     const bill = new Bill({
-      hotWaterConsumption: 0, // TODO
-      coldWaterConsumption: 0,
-      heatConsumption: 0,
+      hotWaterConsumption: consumptions.hotWater,
+      coldWaterConsumption: consumptions.coldWater,
+      heatConsumption: consumptions.heat,
       summary: summary.id,
       user: user.id,
     });
