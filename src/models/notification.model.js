d59aaa62d1b0427e2e07a27e27805880a5758d5d
Balint Soos <soosbalint95@gmail.com>
2017-11-16 01:37:24 +0100

    Fix circular dependency issue

--- src/models/notification.model.js
+++ src/models/notification.model.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const wss = require('../config/wss');
-
 const { Types } = mongoose.Schema;
 
 const notificationSchema = new mongoose.Schema({
@@ -39,7 +37,11 @@ notificationSchema.methods.getPayload = function() {
 notificationSchema.post('save', (notification) => {
   const id = notification.user.toString();
 
-  wss.sendToUser(id, notification.getPayload());
+  wss.sendToUser(id, notification.getPayload()); // eslint-disable-line no-use-before-define
 });
 
 module.exports = mongoose.model('Notification', notificationSchema);
+
+// Fix circular dependency issue
+// https://medium.com/@robonyong/dealing-with-circular-dependencies-mongoose-in-node-js-36f3b24eb015
+const wss = require('../config/wss');
