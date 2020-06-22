cbbed761a72c28de47060b6d32b3cfba32b9be9c
Balint Soos <soosbalint95@gmail.com>
2020-06-22 19:52:31 +0200

    remove express-winston

--- src/processes/web/app.js
+++ src/processes/web/app.js
@@ -1,25 +1,19 @@
 const express = require('express');
-const logger = require('morgan');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const compress = require('compression');
 const methodOverride = require('method-override');
-const expressWinston = require('express-winston');
 const helmet = require('helmet');
 const pdf = require('express-pdf');
 
-const config = require('../../config/config');
 const cors = require('../../config/cors');
 const auth = require('../../config/auth');
-const winstonInstance = require('../../config/winston');
 const router = require('./routers/main.router');
 
 const app = express();
 
-if (config.env === 'development') {
-  app.use(logger('dev'));
-}
-
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -29,19 +23,6 @@ app.use(helmet());
 app.use(cors());
 app.use(pdf);
 app.use(auth.initialize());
-
-// enable detailed API logging in dev env
-if (config.env === 'development') {
-  expressWinston.requestWhitelist.push('body');
-  expressWinston.responseWhitelist.push('body');
-
-  app.use(expressWinston.logger({
-    winstonInstance,
-    meta: true, // optional: log meta data about request (defaults to true)
-    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-    colorStatus: true, // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
-  }));
-}
 
 app.use('/api/v1', router);
 
